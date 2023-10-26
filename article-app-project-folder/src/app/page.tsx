fe329f380a8735946b1b19bd0a7eb9455784f58c
lilium-mdhq <54962572+liliumholden@users.noreply.github.com>
2023-10-25 22:55:15 -0700

    Removing old folder

--- article-app-project-folder/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import styles from './styles/page.module.css'
-import Article from '/components/Article.js'
-
-export default function Home() {
-  return (
-    <Article></Article>
-  )
-}
